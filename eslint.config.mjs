ec9cf52e4321c7b526599d34661f79c12d7c1012
Elaine Chao <arfydogs7@gmail.com>
2025-11-09 15:31:28 -0800

    set up eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,24 @@
 import js from "@eslint/js";
 import globals from "globals";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import jsdoc from 'eslint-plugin-jsdoc';
 
 export default defineConfig([
-  jsdoc.configs['flat/recommended'], 
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js, jsdoc }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  pluginReact.configs.flat.recommended,
+
+  {
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    }
+  },
 
   {
     name: "syntax-rules",
-		rules: {
-			semi: "error",
-		},
-	},
-
-  {
-    name: "enforce-jsdocs", 
     rules: {
-      'jsdoc/require-description': 'error',
-      'jsdoc/require-param': 'error',
-      'jsdoc/require-returns': 'error',
-      'jsdoc/valid-types': 'error',
-    }
-  }
+      semi: "error",
+    },
+  },
 ]);
