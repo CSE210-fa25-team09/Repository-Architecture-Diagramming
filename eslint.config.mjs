5301e75c26120be6698f14bd66265a32d12f3a79
tutingjun <50512161+tutingjun@users.noreply.github.com>
2025-11-11 15:06:45 -0800

    Merge branch 'pipeline-tests' into feat/homepage-layout

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import jsdoc from 'eslint-plugin-jsdoc';
+
+export default defineConfig([
+  jsdoc.configs['flat/recommended'], 
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js, jsdoc }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+
+  {
+    name: "syntax-rules",
+		rules: {
+			semi: "error",
+		},
+	},
+
+  {
+    name: "enforce-jsdocs", 
+    rules: {
+      'jsdoc/require-description': 'error',
+      'jsdoc/require-param': 'error',
+      'jsdoc/require-returns': 'error',
+      'jsdoc/valid-types': 'error',
+    }
+  }
+]);
--- frontend/prettier.config.mjs
+++ frontend/prettier.config.mjs
@@ -8,6 +8,6 @@ const config = {
   trailingComma: "all",
   printWidth: 90,
   tabWidth: 2,
-}
+};
 
-export default config
+export default config;
