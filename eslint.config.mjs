7603cab5bd52de7a5c8a35c671c0fea8afe62a8e
Elaine Chao <arfydogs7@gmail.com>
2025-11-10 23:10:23 -0800

    split eslint config into root and frontend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 import jsdoc from 'eslint-plugin-jsdoc';
 
 export default defineConfig([
   jsdoc.configs['flat/recommended'],
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js, jsdoc }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  pluginReact.configs.flat.recommended,
-
-  {
-    "settings": {
-      "react": {
-        "version": "detect"
-      }
-    }
-  },
 
   {
     name: "syntax-rules",
