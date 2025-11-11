56927b7629cc364fcc31d2f2549969e6fe1c15f2
Elaine Chao <arfydogs7@gmail.com>
2025-11-10 23:01:02 -0800

    added jsdocs enforcement

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,11 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import jsdoc from 'eslint-plugin-jsdoc';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  jsdoc.configs['flat/recommended'], 
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js, jsdoc }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   pluginReact.configs.flat.recommended,
 
   {
@@ -21,4 +23,14 @@ export default defineConfig([
 			semi: "error",
 		},
 	},
+
+  {
+    name: "enforce-jsdocs", 
+    rules: {
+      'jsdoc/require-description': 'error',
+      'jsdoc/require-param': 'error',
+      'jsdoc/require-returns': 'error',
+      'jsdoc/valid-types': 'error',
+    }
+  }
 ]);
